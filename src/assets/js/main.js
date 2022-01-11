16cfbf1b376275e07264affda5b8ccef7cb63b8d
Pavkata4007 <pavel.b.nyagolov@gmail.com>
2022-01-11 12:53:37 +0200

    uma Project

---
+++ src/assets/js/main.js
@@ -0,0 +1,13 @@
+/*
+ * Polyfills
+ */
+import './polyfills/object-fit';
+
+/*
+ * Modules
+ */
+
+import './modules/handle-mobile-nav';
+import './modules/handle-scroll';
+import './modules/handle-select-box';
+import './modules/handle-filter';
